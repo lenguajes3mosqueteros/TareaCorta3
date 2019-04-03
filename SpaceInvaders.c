0b81293858a9e40972b3d863771fe368fa7ac2af
Mariano <marianomm@rocketmail.com>
2019-04-03 17:05:18 -0600

    First Commit

---
+++ SpaceInvaders.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	printf("Space Invaders");
+	/** The comments in English
+	Document each function you do
+	**/ 
+
+	/** Start: April 3, 2019 **/
+
+	printf("Let's do it\n");
+
+}
