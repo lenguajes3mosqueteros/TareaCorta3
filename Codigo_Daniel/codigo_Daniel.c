61d7887c03c52d4889192fca0b5267a47cc97b60
Java-Sith <prieto.luisdaniel@gmail.com>
2019-04-11 17:22:44 -0600

    Update codigo_Daniel.c

--- Codigo_Daniel/codigo_Daniel.c
+++ Codigo_Daniel/codigo_Daniel.c
@@ -363,6 +363,237 @@ void dibujarJuegoTerminado() {
 	SDL_BlitSurface(imagenJuegoTerminado, &fuente, pantalla, &destino);
 }
 
+void velocidadInvasores() {
+    switch (invasores.asesinado)
+    {
+        case 10:
+            invasores.velocidad = 2;
+            invasores.estadoVelocidad = 800;
+            break;
+        case 20:
+            invasores.velocidad = 4;
+            invasores.estadoVelocidad = 600;
+            break;
+        case 30:
+            invasores.velocidad = 2;
+            invasores.estadoVelocidad = 800;
+            break;
+    }
+}
+
+int moverBalas(struct bala_t* b, int max, int velocidad) {
+    for(int i = 0; i < max; i++) {
+        if (b[i].vivo == 1) {
+            b[i].vida.y += velocidad;
+            if (b[i].vida.y <= 0) {
+                b[i].vivo = 0;
+            }
+            else if (b[i].vida.y + b[i].vida.y >= ALTO) {
+                b[i].vivo = 0;
+            }
+        }
+    }
+    return 0;
+}
+
+void moverInvasoresVertical() {
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 10; j++) {
+            invasores.enemigos[i][j].vida.y += 15;
+        }
+    }
+}
+
+int moverInvasores(int velocidad) {
+    velocidadInvasores();
+    switch (invasores.direccion)
+    {
+        case izquierda:
+            for(int i = 0; i < 10; i++) {
+                for(int j = 0; j < 5; j++){
+                    if (invasores.enemigos[j][i].vivo == 1) {
+                        if (invasores.enemigos[j][i].vida.x <= 0) {
+                            invasores.direccion = derecha;
+                            moverInvasoresVertical();
+                            return 0;
+                        }
+                        if (invasores.estadoTiempo + invasores.estadoVelocidad < SDL_GetTicks()) {
+                            invasores.estadoTiempo = SDL_GetTicks();
+                            if (invasores.estado == 1) {
+                                invasores.estado = 0;
+                            } else {
+                                invasores.estado = 1;
+                            }
+                        }
+                        invasores.enemigos[i][j].vida.x -= invasores.velocidad;
+                    }
+                }
+            }
+            break;
+        case derecha:
+            for(int i = 9; i >= 0; i--) {
+                for(int j = 0; j < 5; i++) {
+                    if (invasores.enemigos[j][i].vivo == 1) {
+                        if (invasores.enemigos[j][i].vida.x + ANCHO_E >= ANCHO) {
+                            invasores.direccion = izquierda;
+                            moverInvasoresVertical();
+                            return 0;
+                        }
+                    } 
+                   if (invasores.estadoTiempo + invasores.estadoVelocidad < SDL_GetTicks()) {
+                        invasores.estadoTiempo = SDL_GetTicks();
+                        if (invasores.estado == 1) {
+                            invasores.estado = 0;
+                        } else {
+                            invasores.estado = 1;
+                        }
+                    }
+                    invasores.enemigos[i][j].vida.x -= invasores.velocidad;
+                }
+            }
+        default:
+            break;
+    }
+    return 0;
+}
+
+void moverJugador(enum direcciones_t direccion) {
+    if (direccion == izquierda) {
+        if (jugador.vida.x > 0) {
+            jugador.vida.x -= 10;
+        }
+    }
+    else if (direccion == derecha) {
+        if (jugador.vida.x + jugador.vida.w < ANCHO) {
+            jugador.vida.x += 10;
+        }
+    }
+}
+
+void moverPlatillo() {
+    if (platillo.vivo == 1) {
+        if (platillo.direccion == izquierda) {
+            platillo.vida.x -= 5;
+            if (platillo.vida.x < 0) {
+                platillo.vivo = 0;
+                platillo.vida.x = 0;
+                platillo.direccion = derecha;
+            }
+        }
+        if (platillo.direccion == derecha) {
+            platillo.vida.x += 5;
+            if (platillo.vida.x + platillo.vida.w > ANCHO) {
+                platillo.vivo = 0;
+                platillo.vida.x = ANCHO - platillo.vida.w;
+                platillo.direccion = izquierda;
+            }
+        } 
+    }
+}
+
+int colision(SDL_Rect a, SDL_Rect b) {
+    if (a.y + a.h < b.y) {
+		return 0;
+	}		
+	if (a.y > b.y + b.h) {		
+		return 0;
+	}		
+	if (a.x > b.x + b.w) {		
+		return 0;
+	}	
+	if (a.x + a.w < b.x) {		
+		return 0;
+	}
+	return 1;
+}
+
+void dañoBala(struct base_t* base, int b, struct bala_t* bala, int l) {
+    int x, y;
+    SDL_Rect fuente, destino;
+    SDL_LockSurface(imagenBase[b]);
+    Uint8* pixeles = (Uint8*)imagenBase[b]->pixels;
+    int pix;
+    if (l == 0) {
+        x = (bala->vida.x + 3) - base->vida.x;
+        y = base->vida.y - 1;
+        for(int i = 0; i < base->vida.h; i++) {
+            if (x >= ANCHO_BASE) {
+                x = ANCHO_BASE - 1;
+            }
+            pix = y * imagenBase[b]->pitch + x;
+            if (pixeles[pix] != 200) {
+                bala->vivo = 0;
+                fuente.x = 0;
+				fuente.y = 0;
+				fuente.w = 11;
+				fuente.h = 15;
+				destino.x = x - 3;
+				destino.y = y - 14;
+				destino.w = 11;
+				destino.h = 15;
+                SDL_UnlockSurface(imagenBase[b]);
+                SDL_BlitSurface(imagenDaño, &fuente, imagenBase[b], &destino);
+                break;
+            }
+            y--;
+        } 
+    }
+    if (l == 1) {
+        x = (bala->vida.x + 3) - base->vida.x;
+        y = 0;
+        for(int i = 0; i < base->vida.h; i++) {
+            if (x >= ANCHO_BASE) {
+                x = ANCHO_BASE - 1;
+            }
+            pix = y * imagenBase[b]->pitch + x;
+            if (pixeles[pix] != 200) {
+                bala->vivo = 0;
+                fuente.x = 0;
+				fuente.y = 0;
+				fuente.w = 11;
+				fuente.h = 15;
+				destino.x = x - 3;
+				destino.y = y - 14;
+				destino.w = 11;
+				destino.h = 15;
+                SDL_UnlockSurface(imagenBase[b]);
+                SDL_BlitSurface(imagenDaño, &fuente, imagenBase[b], &destino);
+                break;
+            }
+            y++;
+        } 
+    }
+    SDL_UnlockSurface(imagenBase[b]);
+}
+
+void dañoEnemigo(struct enemigo_t* enemigo, struct base_t* base, int indice) {
+    int x, y;
+    SDL_Rect destino;
+    if (invasores.direccion == derecha) {
+        x = (enemigo->vida.x + invasores.velocidad) - base->vida.x;
+        y = enemigo->vida.y - base->vida.y;
+        if (x > 0) {
+            destino.x = 0;
+            destino.y = y;
+            destino.w = x;
+            destino.h = enemigo->vida.h;
+            SDL_FillRect(imagenBase[indice], &destino, 227);
+        }
+    }
+    else if (invasores.direccion == izquierda) {
+        x = (enemigo->vida.x + (enemigo->vida.w - 1)) + invasores.velocidad;
+        x = x - base->vida.x;
+        y = enemigo->vida.y - base->vida.y;
+        if (x < base->vida.w) {
+            destino.x = x;
+            destino.y = y;
+            destino.w = base->vida.w - 1;
+            destino.h = enemigo->vida.h;
+            SDL_FillRect(imagenBase[indice], &destino, 227);
+        }
+    }
+}
+
 int main(){
 	printf("Hola Daniel");
 	return 0;
